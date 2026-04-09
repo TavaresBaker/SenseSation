c643226f14088dcd0e7a0a955b03bcc83baa5fc6
Will-Mickelson <WRMickelson01@indianatech.net>
2026-04-09 19:19:53 -0400

    Update restore_files.sh

--- Scripts/restore_files.sh
+++ Scripts/restore_files.sh
@@ -5,7 +5,7 @@
 
 # Detect pfSense version
 VERSION_RAW=$(cat /etc/version | cut -d'-' -f1)
-BRANCH="RELENG_$(echo "$VERSION_RAW" | tr '.' '_')"
+BRANCH="master$(echo "$VERSION_RAW" | tr '.' '_')"
 
 # Fallback options if main branch fails
 BRANCHES="$BRANCH master main"
